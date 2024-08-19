d5f468ac57c2f324bc4c6564b9415d104c6132e1
tolly00 <albertosylveus@gmail.com>
2024-08-19 13:07:47 -0400

    logo

--- blog/settings.py
+++ blog/settings.py
@@ -118,7 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
+
+# settings.py
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Ce répertoire inclut les fichiers statiques du projet
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
